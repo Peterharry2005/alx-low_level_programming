aa567bddea0f9af1b706c043a7b0482d9d7f2813
Sharia Peter <Peteemaris@gmail.com>
2023-03-16 19:10:58 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ printf("size of a char: %1u byte(s)\n", (unsigned long)sizeof(a));
 printf("size of an int: %1u byte(s)\n", (unsigned long)sizeof(b));
 printf("size of a long int: %1u byte(s)\n", (unsigned long)sizeof(c));
 printf("size of a long long int: %1u byte(s)\n", (unsigned long)sizeof(d));
-printf("size of afloat: &1u byte(s)\n", (unsigned long)sizeof(f));
+printf("size of a float: &1u byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
